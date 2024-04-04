faaaaeda9efadfebf9e20a3b251292d0f4c4053c
Adam Zhang <adamzhang.aus@gmail.com>
2024-04-04 16:11:59 +1100

    feat: add click listeners to numbers and clear buttons

--- main.ts
+++ main.ts
@@ -1,4 +1,17 @@
 const numberBtns = document.getElementsByClassName("number");
+const calcScreen = document.getElementsByClassName("screen")[0];
+
+let total = 0;
+
+// add event listeners
 for (let i = 0; i < numberBtns.length; i ++) {
-    console.log(numberBtns[i].innerHTML);
+    numberBtns[i].addEventListener("click", () => {
+        calcScreen.innerHTML = calcScreen.innerHTML + numberBtns[i].innerHTML;
+    })
 }
+
+const clearBtn = document.getElementById("clear-button");
+clearBtn?.addEventListener("click", () => {
+   total = 0;
+   calcScreen.innerHTML = ""; 
+})
